d66f727e0ef2ed2f6ea3a9dcccc71c94ec3c59fb
alex <a.davies95@gmail.com>
2025-11-14 15:41:02 +0000

    App exists

---
+++ Government/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Government
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Government";
+
+            Console.ReadKey();
+        }
+    }
+}
